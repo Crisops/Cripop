1a98309dfd7f6ef8ea9679d5d4572a08a63c2c7e
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-11-23 22:37:10 -0500

    Add types for search movie and filters. Fetching data movies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Crisops.github.io',
   base: '/Cripop',
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org'
+      }
+    ]
+  },
   integrations: [tailwind(), react()]
-});
+})
