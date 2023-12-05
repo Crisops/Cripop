141edc36b152eba32d2354820cfa202ccba0532e
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-12-05 12:39:51 -0500

    Change URL tag a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Crisops.github.io',
-  base: '/Cripop',
   image: {
     remotePatterns: [{
       protocol: 'https',
@@ -17,4 +15,4 @@ export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
   adapter: vercel()
-});
+})
