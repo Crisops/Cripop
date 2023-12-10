962027732e568e1e4d1b89de6208fa7e22c62577
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-12-10 12:23:09 -0500

    Add model Box Season Series with out interactivity

---
+++ src/components/react/BoxSeasonSeries.jsx
@@ -0,0 +1,7 @@
+const BoxSeasonSeries = () => {
+  return (
+    <div className='relative w-full h-auto border border-red-600 transition duration-500 cursor-pointer' />
+  )
+}
+
+export default BoxSeasonSeries
