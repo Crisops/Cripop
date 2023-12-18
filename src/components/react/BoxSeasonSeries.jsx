57eed2d8217989acfb19ba5c02a267da4d133f22
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2023-12-18 15:46:58 -0500

    STYLE link back not found search

--- src/components/react/BoxSeasonSeries.jsx
+++ src/components/react/BoxSeasonSeries.jsx
@@ -5,7 +5,7 @@ const BoxSeasonSeries = ({ children }) => {
   const { isView } = useViewSeasons(state => state)
 
   return (
-    <div id='scroll' className={`relative flex flex-col ${isView ? 'w-[350px] sm:w-[610px] md:w-[740px] xl:w-[500px]' : 'w-0'} h-[300px] xl:h-[500px] bg-white/10 overflow-y-scroll transition-all duration-500 ease xl:backdrop-blur-sm`}>
+    <div id='scroll' className={`relative flex flex-col ${isView ? 'w-[350px] sm:w-[610px] md:w-[740px] xl:w-[500px]' : 'w-0'} h-[300px] xl:h-[500px]  bg-[#000105] xl:bg-white/10 overflow-y-scroll transition-all duration-500 ease xl:backdrop-blur-sm`}>
       {children}
     </div>
   )
