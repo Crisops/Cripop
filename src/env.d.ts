0baaf9c5b763c3434a0c40809b33eb4e1ae13fb9
Crisops <crialeperez1835@gmail.com>
2025-08-23 10:05:29 -0500

    (feat) Add configuration files for Prettier, update dependencies, and adjust Astro settings.
    -  A new hoisting pattern for @heroui dependencies is included.
    -Update versions of React and TypeScript are updated.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SECRET_API_KEY: string;
+  readonly SECRET_TOKEN_ACCESS: string;
+  readonly TMDB_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
