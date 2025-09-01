befde3b238664f46f837df76d4587a98adc1afd8
Crisops <crialeperez1835@gmail.com>
2025-09-01 13:09:50 -0500

    (feat) Added new interfaces and types for media details.
    - Added the `DetailsSeriesSeasons` interface to include information about the current season and duration.
    - Updated the `ResultSearch` type and introduced the `MediaDetails` type to handle details for movies and series.

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -1,4 +1,13 @@
-import type { Movies } from "@/types/movies";
-import type { Series } from "@/types/series";
+import type { Movies } from '@/types/movies'
+import type { Series } from '@/types/series'
+import type { DetailsSeries as DetailsSeriesId } from '@/types/details-serie'
+import type { DetailsMovieId } from '@/types/details-movie-id'
+import type { DetailsSeason } from '@/types/data-season'
 
-export type ResultSearch = Movies | Series;
+export interface DetailsSeriesSeasons extends DetailsSeriesId {
+  currentSeason: DetailsSeason
+  runtime: number
+}
+
+export type ResultSearch = Movies | Series
+export type MediaDetails = DetailsMovieId | DetailsSeriesId | DetailsSeriesSeasons
