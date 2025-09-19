b0c231f3fe13ee0fbe3e9751d19af36121d62402
Crisops <crialeperez1835@gmail.com>
2025-09-18 20:47:13 -0500

    (feat) Creation of function for initial filter forms.
    - The `createInitialFormFilters` function has been added to generate initial forms according to content type (movie or series).
    - Utilities from `@internationalized/date` have been imported to handle dates and time zones, improving filter management in searches.

--- src/config/initial-forms.ts
+++ src/config/initial-forms.ts
@@ -1,6 +1,41 @@
-import type { FormSearch } from "@/utils/validate-rules-form";
+import { getLocalTimeZone, today } from '@internationalized/date'
+import { SortDate } from '@/types/filter-options'
+import type { FormFiltersOptions, FormSearch } from '@/utils/validate-rules-form'
 
 export const initialFormSearch: FormSearch = {
-  search: "",
-  type: "movie",
-};
+  search: '',
+  type: 'movie',
+}
+
+// Función helper para crear formulario inicial según el tipo
+export const createInitialFormFilters = <T extends FormSearch['type']>(contentType: T): FormFiltersOptions<T> => {
+  if (contentType === 'movie') {
+    return {
+      primary_release_date: {
+        gte: today(getLocalTimeZone()).subtract({ days: 1 }),
+        lte: today(getLocalTimeZone()).add({ days: 1 }),
+      },
+      with_runtime: {
+        gte: 0,
+        lte: 0,
+      },
+      with_genres: '',
+      with_original_language: '',
+      sort_by: SortDate.POPULARITY_DESC,
+    } as FormFiltersOptions<T>
+  } else {
+    return {
+      first_air_date: {
+        gte: today(getLocalTimeZone()).subtract({ days: 1 }),
+        lte: today(getLocalTimeZone()).add({ days: 1 }),
+      },
+      with_runtime: {
+        gte: 0,
+        lte: 0,
+      },
+      with_genres: '',
+      with_original_language: '',
+      sort_by: SortDate.POPULARITY_DESC,
+    } as FormFiltersOptions<T>
+  }
+}
