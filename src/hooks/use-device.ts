322b08db70a0fcaf2ccf1f817447b1ce1fba4830
Crisops <crialeperez1835@gmail.com>
2025-09-18 20:45:56 -0500

    (feat) Improved viewport handling and form optimization.
    - Added the `debounce` function to optimize the handling of the window resizing event in the `useDevice` hook.
    - Adjustments have been made to the `useForm` hook to include new states such as `isDirty`, `trigger`, and `getValues`, improving form management.

--- src/hooks/use-device.ts
+++ src/hooks/use-device.ts
@@ -1,22 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import debounce from 'lodash-es/debounce'
 
 export const useDevice = () => {
   const [viewport, setViewport] = useState({
     width: typeof window !== 'undefined' ? window.innerWidth : 0,
   })
 
-  useEffect(() => {
-    const handleResize = () => {
-      setViewport({
-        width: window.innerWidth,
-      })
-    }
-
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
+  const updateViewport = useCallback(() => {
+    setViewport({
+      width: window.innerWidth,
+    })
   }, [])
+
+  const debouncedHandleResize = useCallback(debounce(updateViewport, 150), [updateViewport])
+
+  useEffect(() => {
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize)
+      debouncedHandleResize.cancel()
+    }
+  }, [debouncedHandleResize])
 
   return {
     isMobile: viewport.width < 768,
